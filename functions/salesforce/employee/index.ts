7010b68abfc93b1c938c95da1a0e42f79e161128
ShrekrishAG <shreya@dollfamilyoffice.com>
2026-02-26 11:02:05 -0600

    deleting duplicated file paths

--- functions/salesforce/employee/index.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response } from 'express'
-
-export default async function handler(req: Request, res: Response) {
-  return res.json({
-    ok: true,
-    path: req.path,
-    method: req.method
-  })
-}
